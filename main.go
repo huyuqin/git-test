c8ad8ef3cf0a90443cafce171e1cbc73d160cf7f
huyuqin <hyq13173682102@163.com>
2022-07-08 14:58:09 +0800

    hot 2 commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("hahahhaaha")
 	fmt.Println("我修改了代码")
 	fmt.Println("这是hot分支")
+	time.Sleep(time.Second)
 }
